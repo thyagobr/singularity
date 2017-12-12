7b76e9d12155defedf6674f78e977c5bcfa7649c
Thyago Barbosa Rodrigues <thyagobr@gmail.com>
2017-12-12 11:16:32 -0300

    remakes singularity view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def activity_time_for_task(task_id)
+    elapsed_seconds = Activity.where(task_id: task_id).reduce(0) do |memo, activity|
+      memo += activity.finished_at - activity.started_at
+    end.to_i
+    result = []
+    minutes, seconds = elapsed_seconds.divmod(60)
+    result << "#{seconds} segundos"
+    if minutes > 0
+      result << "#{minutes} minutos"
+      hours, minutes = minutes.divmod(60)
+      if hours > 0
+        result << "#{days} hours"
+        days, hours = hours.divmod(24)
+        result << "#{days} dias" if days > 0
+      end
+    end
+    result.reverse.join(", ")
+  end
 end
