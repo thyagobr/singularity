0d22d3828bffa169150776c2b10afed72dd90192
Thyago Barbosa Rodrigues <thyagobr@gmail.com>
2017-12-12 15:45:33 -0300

    changes displaying order of tasks

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   def index
     @task = Task.new
     @current_task = Activity.find_by(finished_at: nil)&.task
-    @tasks = Task.all - [@current_task].compact
+    @tasks = Task.includes(:activities).order('activities.created_at desc') - [@current_task].compact
   end
 end
