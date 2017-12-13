bd016dbb30882b37096d81d7593b9fc31b0748dd
Thyago Barbosa Rodrigues <thyagobr@gmail.com>
2017-12-13 10:12:43 -0300

    removes reference do dashboard_url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to dashboard_url
+      redirect_to root_url
     else
       redirect_to login_url
     end
